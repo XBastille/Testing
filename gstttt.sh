de89319a06015f0c9e93ccbddc4f5707dcb49d36
XBastille <148121344+XBastille@users.noreply.github.com>
2024-05-18 12:34:03 +0530

    Add files via upload

---
+++ gstttt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "installing Gstreamer lol"
+wget https://github.com/XBastille/Testing/releases/download/Gstreamer/gst.tar.xz
+tar xf gst.tar.xz
+echo "Installed now dance"
+rm -rf gst.tar.xz
